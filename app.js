303b5d1bab64a5eac9da8af630c6da83048cb051
winstonlin0309 <winstonlin0309@gmail.com>
2023-07-12 21:25:40 -0700

    create lists

--- app.js
+++ app.js
@@ -1,45 +1,28 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+main().catch(err => console.log(err));
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+async function main() {
+  app.set("view engine", 'ejs');
+  app.use(bodyParser.urlencoded({extended:true}));
+  app.use(express.static('public'));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+  app.get("/", (req, res) => {
+    res.render("list", {listTitle : "Today"});
+  })
 
-app.get("/", function(req, res) {
+  
+  
 
-  res.render("list", {listTitle: day, newListItems: items});
+  app.get("/about", (req, res) => {
+    res.render("about");
+  });
 
-});
-
-app.post("/", function(req, res){
-
-  const item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
-});
-
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
-});
-
-app.get("/about", function(req, res){
-  res.render("about");
-});
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+  app.listen(3000, () => {
+    console.log("Connect Successfully");
+  });
+}// end of main function
